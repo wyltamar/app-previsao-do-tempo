ce0acc2f0964c43e89198fde8576d22f3f1a829e
Wyltamar <wyltamar.douglasdesousaoliveir@gmail.com>
2026-01-29 06:29:35 -0300

    Escripts.js incompleto

---
+++ scripts.js
@@ -0,0 +1,23 @@
+async function preverTempo() {
+
+    const apiKey = "58c8fa2c58187081b787ed0121d89211"
+
+    let cidade = document.querySelector(".input-cidade").value;
+    let caixaMedia = document.querySelector(".caixa-media");
+
+
+    const urlApi = `https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${apiKey}&units=metric&lang=pt_br`;
+
+    let respostaServidor = await fetch(urlApi)
+
+    let dados = await respostaServidor.json();
+
+    caixaMedia.innerHTML = `
+        <h2 class="cidade">${dados.name}</h2>
+        <p class="temperatura">Temperatura: ${Math.floor(dados.main.temp)} °C</p>
+        <img src="http://openweathermap.org/img/wn/${dados.weather[0].icon}.png" alt="Ícone do tempo">
+        <p class="umidade">Umidade: ${dados.main.humidity} %</p>
+        <button class="botao-IA" onclick="respostaIA()">Sugestão de Roupa</button>
+        <p class="resposta-IA">Sugestão de roupa</p>
+    `
+}
